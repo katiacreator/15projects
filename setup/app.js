efb2b0012bc5d6bcae449dc3416e936eaae2e67a
Katia Gibson <kcygibson@gmail.com>
2021-06-20 13:43:39 -0400

    created all the html and the first part of the js to get this project working initially

---
+++ setup/app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    // get random Number btw 0 and 3 representing colors array index
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber (){
+    return Math.floor(Math.random() * colors.length);
+}
