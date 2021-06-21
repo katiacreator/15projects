cd92226aef8578aa9d9b2c26014530a2b191a8db
Katia Gibson <kcygibson@gmail.com>
2021-06-20 21:27:04 -0400

    got open modal and close modal btn click functionality working

--- modal/app.js
+++ modal/app.js
@@ -2,3 +2,23 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+// toggle clas .open-modal
+//selectors
+const modalBtn = document.querySelector('.modal-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
+const modalContainer = document.querySelector('.modal-container')
+//console.log(modalBtn);
+//console.log(modalOverlay);
+//console.log(closeBtn);
+
+//event listeners
+modalBtn.addEventListener('click', function (){
+    //console.log("clicked");
+    modalOverlay.classList.add('open-modal');
+})
+
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal')
+})
